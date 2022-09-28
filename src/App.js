329c719e4aeea523b508ebb8720fb8ebc171564b
tcocco20 <theococco@yahoo.com>
2022-09-28 09:30:03 -0700

    added thunks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-slice";
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector(state => state.cart.cartIsOpen);
-  const cart = useSelector(state => state.cart);
+  const cart = useSelector(state => state.cartItems);
+  const notification = useSelector(state => state.cart.notification);
 
   useEffect(() => {
-    fetch("https://react-http-6b4a6.firebaseio.com/cart.json", {
-      method: "PUT",
-      body: JSON.stringify(cart),
-    });
-  }, [cart]);
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -4,6 +4,7 @@ const initialState = {
   cartItems: [],
   totalQuantity: 0,
   cartIsOpen: false,
+  notification: null,
 };
 
 const cartSlice = createSlice({
@@ -42,9 +43,62 @@ const cartSlice = createSlice({
     toggleCart(state) {
       state.cartIsOpen = !state.cartIsOpen;
     },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
   },
 });
 
 export const cartActions = cartSlice.actions;
+
+export const sendCartData = cart => {
+  return async dispatch => {
+    dispatch(
+      cartActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-http-6b4a6.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        cartActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        cartActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
 
 export default cartSlice.reducer;
